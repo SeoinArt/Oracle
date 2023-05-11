8a398454e7273066737ceb50d106a3418507f099
seoin2356 <seoin2389@icloud.com>
2023-05-11 17:03:53 +0900

    2023-05-11 커밋

--- Multishop/multishop.sql
+++ Multishop/multishop.sql
@@ -1,13 +1,20 @@
+create table memo(
+    no number(4) primary KEY,
+    name varchar2(30) not null,
+    msg varchar2(100),
+    wdate date default sysdate
+);
+
+desc memo;
+drop sequence memo_seq;
+
+create sequence memo_seq
+start with 1 increment by 1 nocache;
+
+select * from memo;
+
+select * from memberView;
+
 select * from member;
 
-update member set mstate = 9 where idx =6;
-update member set mstate = -1 where idx =4;
-update member set mstate = -2 where idx =5;
-
-commit;
-
-select member.*, 
-decode(mstate,0,'활동회원',-1,'정지회원',-2,'탈퇴회원',9,'관리자') mstateStr from member 
-order by idx desc;
-
-update member set addr2='2번지' where idx =2;
+select * from product;
